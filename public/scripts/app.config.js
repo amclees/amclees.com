294b18110877953aad5f1cc59095ca8dc91170bb
xv435 <xkx63@aol.com>
2016-12-07 10:26:45 -0800

    Fixed template routing, added 404 template.

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -8,17 +8,19 @@ angular.
           templateUrl: '/templates/homepage.template.html'
         }).
         when('/github', {
-          template: '/templates/github.template.html'
+          templateUrl: '/templates/github.template.html'
         }).
         when('/projects', {
-          template: '/templates/projects.template.html'
+          templateUrl: '/templates/projects.template.html'
         }).
         when('/about', {
-          template: '/templates/about.template.html'
+          templateUrl: '/templates/about.template.html'
         }).
         when('/contact', {
-          template: '/templates/contact.template.html'
+          templateUrl: '/templates/contact.template.html'
         }).
-        otherwise('/');
+        otherwise({
+          templateUrl: '/templates/404.template.html'
+        });
     }
 ]);
